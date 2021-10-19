16c26142b15930730a6aca08b728e1205868d010
Kriska939 <cristinacorrado93@gmail.com>
2021-10-19 23:05:55 +0200

    added post index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <h1 class="text-center">Posts</h1>
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Titolo</th>
+            <th scope="col">Data di pubblicazione</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+
+            @forelse($posts as $post)
+                
+            <tr>
+                <td>{{$post->title}}</td>
+                <td>{{$post->created_at}}</td>
+                <td>
+                    <a href="{{route('admin.posts.show', $post->id)}}" class="btn btn-primary">Apri</a>
+                </td>
+              </tr>
+
+            @empty
+            <tr><td colspan="3" class="text-center">Nessun Post Trovato</td></tr>
+              
+            
+            @endforelse
+         
+        </tbody>
+      </table>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest.welcome');
+    return view('guest.home');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// ROTTE CHE RAGGIUNGO SOLO SE SONO LOGGATA
+
+Route::middleware('auth')->name('admin.')->prefix('admin')->namespace('Admin')->group(function(){
+
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+});
+
