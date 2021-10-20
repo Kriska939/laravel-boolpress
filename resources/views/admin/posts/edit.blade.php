6ee61dcdd880385e41c0187ace55c533cc9010d7
Kriska939 <cristinacorrado93@gmail.com>
2021-10-20 23:22:02 +0200

    added COMPLETE CRUD

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,5 +2,29 @@
 
 @section('content')
 
-PAGINA DI EDIT
+<div class="container">
+    <header>
+        <h1>Modifica post</h1>
+    </header>
+    <section id="form">
+        <form method="POST" action="{{ route('admin.posts.update', $post->id)}}">
+            @csrf
+            @method('PATCH')
+            <div class="form-group">
+              <label for="exampleFormControlInput1">Titolo</label>
+              <input type="text" class="form-control" id="title" name="title" value="{{$post->title}}" >
+            </div>
+            <div class="form-group">
+              <label for="content">Contenuto del post</label>
+              <textarea class="form-control" id="content" name="content" rows="5">{{$post->title}}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Immagine</label>
+                <input type="text" class="form-control" id="image" name="image" value="{{$post->image}}">
+              </div>
+
+              <button type="submit" class="btn btn-success">Pubblica</button>
+          </form>
+    </section>
+</div>
 @endsection
