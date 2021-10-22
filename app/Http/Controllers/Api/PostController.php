45b3d1cd657e00272fd7a855e43cdbcf228dd767
Kriska939 <cristinacorrado93@gmail.com>
2021-10-22 12:32:01 +0200

    added complete api controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -27,7 +27,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
@@ -36,9 +36,9 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        return response()->json($post);
     }
 
     /**
@@ -61,6 +61,7 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Post::destroy($id);
+        return response('', 204);
     }
 }
