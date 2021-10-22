a2bed5cd5f0db873da1cfa188679685041f3acdf
Kriska939 <cristinacorrado93@gmail.com>
2021-10-22 16:42:50 +0200

    added simple pagination

--- resources/js/components/Posts/PostList.vue
+++ resources/js/components/Posts/PostList.vue
@@ -2,6 +2,27 @@
   <section id="post-list">
     <h2>I miei post</h2>
     <PostCard v-for="post in posts" :key="post.id" :post="post" class="my-5" />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <a
+            class="page-link"
+            v-if="pagination.currentPage > 1"
+            @click="getPosts(pagination.currentPage - 1)"
+            >Precedente</a
+          >
+        </li>
+
+        <li class="page-item">
+          <a
+            class="page-link"
+            v-if="pagination.lastPage > pagination.currentPage"
+            @click="getPosts(pagination.currentPage + 1)"
+            >Successiva</a
+          >
+        </li>
+      </ul>
+    </nav>
   </section>
 </template>
 
@@ -19,14 +40,18 @@ export default {
   data() {
     return {
       posts: [],
+      pagination: {},
     };
   },
   methods: {
-    getPosts() {
+    getPosts(page) {
       axios
-        .get("http://localhost:8000/api/posts")
+        .get(`http://localhost:8000/api/posts?page=${page}`)
         .then((res) => {
-          this.posts = res.data;
+          const { data, current_page, last_page } = res.data;
+          this.posts = data;
+
+          this.pagination = { currentPage: current_page, lastPage: last_page };
         })
         .catch((e) => {
           console.error(e);
@@ -37,4 +62,10 @@ export default {
     this.getPosts();
   },
 };
-</script>
+</script >
+
+<style scoped>
+.page-link {
+  cursor: pointer;
+}
+</style>
