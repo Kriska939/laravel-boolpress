02f97a0eed967431ab92ba169fac33bfaabba0b7
Kriska939 <cristinacorrado93@gmail.com>
2021-10-24 21:56:48 +0200

    added relation between models (post and category)

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    // definisco la relazione delle categorie con i post: (in questo caso, le categorie possono avere TANTI post)
+
+    public function posts(){
+        $this->hasMany('App\Models\Post');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'content', 'image', 'slug'];
+
+    // definisco la relazione dei post con la categoria: (in questo caso, il post può riferirsi SOLO a una categoria)
+
+    public function category(){
+
+        return $this->belongsTo('App\Models\Category');
+    }
 }
+
